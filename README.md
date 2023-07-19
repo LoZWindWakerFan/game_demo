7f3cfe358155298cebcf66880d3f92ebad6822c5
LoZWindWakerFan <hannacieslik2000@gmail.com>
2023-07-19 15:22:43 +0200

    Make a window

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-----README----
+# ----README-datei----
+
+#### was ist das?
+das ist einfach ein prototyp für ein spiel das ich später machen will
+TODO: das auf englisch schreiben
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-#print("hallo welt")
+import pygame as pg
+from sys import exit
+
+pg.init()
+
+#Window 
+screen = pg.display.set_mode((850, 500))
+pg.display.set_caption("Prototype/Demo")
+
+#Clock
+clock = pg.time.Clock()
+
+#Game loop
+active = True
+while active:
+    for event in pg.event.get():
+        if event.type ==pg.QUIT:
+            pg.quit()
+            exit()
+
+
+pg.display.update()
+clock.tick(60)
