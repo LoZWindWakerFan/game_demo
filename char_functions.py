a5fe41dbb57e46c296d6a27aa53cd8fdc45a9899
LoZWindWakerFan <hannacieslik2000@gmail.com>
2023-07-23 19:30:22 +0200

    Added rectangles to screen

---
+++ char_functions.py
@@ -0,0 +1,5 @@
+import pygame as pg
+
+#player
+player = pg.Surface((170,50))
+player.fill("Red")
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame as pg
 from sys import exit
 
+from char_functions import *
+
 pg.init()
 
 #Window 
@@ -10,6 +12,10 @@ pg.display.set_caption("Prototype/Demo")
 #Clock
 clock = pg.time.Clock()
 
+#Surfaces
+skybox = pg.Surface((850,500))
+skybox.fill("Light Blue")
+
 #Game loop
 active = True
 while active:
@@ -17,7 +23,13 @@ while active:
         if event.type ==pg.QUIT:
             pg.quit()
             exit()
+    
+    #Surfaces 
+    screen.blit(skybox, (0,0))
+    screen.blit(player, (50,0))
+    screen.blit(player, (450,0))
+    screen.blit(player, (250, 450))
 
 
-pg.display.update()
-clock.tick(60)
+    pg.display.update()
+    clock.tick(60)
